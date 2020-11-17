a62ab31409ae9417b8bad4a539dae09f0ea96006
Pooja <pmasand18@gmail.com>
2020-11-17 16:49:09 +0100

    Implementation of Post Mapping

---
+++ frontend/src/components/posts/PostForm.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+
+function PostForm({onSubmit}) {
+    const [textBody, setTextBody] = useState("");
+
+
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h4 className="card-title" >Add Post</h4>
+                <div>
+                    <div className="form-group">
+                        <label>Body:</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="..."
+                            value={textBody}
+                            onChange={e => setTextBody(e.target.value)} />
+                    </div>
+
+
+
+                    <div className="form-group">
+                        <button
+                            className="btn btn-info"
+                            onClick={() => onSubmit({textBody})}>
+                            Login
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PostForm;
