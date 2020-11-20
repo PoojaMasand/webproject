df6e5c045544b41d00672ce5f60cb1a9245f736a
tristanmcc <tristanmccarthy085@gmail.com>
2020-11-20 09:35:36 +0100

    Comment button styling

--- frontend/src/components/comment/TrendingComments.js
+++ frontend/src/components/comment/TrendingComments.js
@@ -1,16 +1,34 @@
-import CommentCard from "./CommentCard"
+import CommentCard from "./CommentCard";
 
-function TrendingComments({comments, onCommentDelete}) {
+function TrendingComments({ comments, onCommentDelete }) {
+  console.log("Inside Trending Comments" + comments);
 
-    console.log("Inside Trending Comments" + comments)
+  return (
+    <div>
+      <div>
+        <a
+          class="btn btn-danger mb-4"
+          data-toggle="collapse"
+          href="#collapseExample"
+          role="button"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
+          View Comments
+        </a>
+      </div>
+      <div class="collapse" id="collapseExample">
+        <div className="card card-body mt-4 mb-4 bg-dark text-light">
+        <h3>Comments</h3>
+        {comments.map((item) => (
+          <CommentCard key={comments.id} comment={item} />
+        ))}
+      </div>
+        
+      </div>
 
-    return (
-    <div className="card mt-4 mb-4 bg-dark text-light">
-            <h3>Comments</h3>
-                    {comments.map((item) => <CommentCard key={comments.id} comment = {item} />)}
-        </div>
-
-    );
+    </div>
+  );
 }
 
 export default TrendingComments;
